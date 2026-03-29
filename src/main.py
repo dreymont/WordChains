8f4df0009af3d37fbd7224a8ee44172dcd8577a1
dreymont <dreymont@grupazpr.pl>
2026-03-29 12:07:09 +0200

    New folders

---
+++ src/main.py
@@ -0,0 +1,8 @@
+
+#
+# with open("src/ScrabbleWords.txt") as f:
+#     keywords = tuple(f.read().split('\n'))
+
+
+def word_chains():
+    pass
---
+++ test/test_wordchains.py
@@ -0,0 +1,6 @@
+from src.main import word_chains
+import pytest
+
+
+def test_word_chains():
+    pass
